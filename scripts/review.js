2f820e1b316b5e1bd64c477caf4f080d407560fe
bolimarco26 <bolimarco26@gmail.com>
2026-08-05 21:03:22 -0400

    1st attemp

---
+++ scripts/review.js
@@ -0,0 +1,10 @@
+let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+reviewCount++;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+document.querySelector("#reviewCount").textContent = reviewCount;
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent = document.lastModified;
